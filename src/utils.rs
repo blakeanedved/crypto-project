32974a66e5f13cc61066372db59482c9e163137b
Aaron Ingalls <aaroningalls@hotmail.com>
2022-04-27 23:36:32 -0500

    Implemented key expansion

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,9 @@
+use crate::aes;
 use crate::rsa;
 use rug::Integer;
 
 pub fn test() {
-    let (public, private) = rsa::rsa_key_gen();
-    println!("public={:?} private={:?}", public, private);
-
-    let c = rsa::rsa_encrypt(&public, Integer::from(4u32));
-    println!("{}", c);
-
-    let m = rsa::rsa_decrypt(&private, c);
-
-    println!("{}", m);
+    let x = aes::AES::new();
+    // x.key_expansion();
+    println!("{:x?}", x.key_expansion())
 }
