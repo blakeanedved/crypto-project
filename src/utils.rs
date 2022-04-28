86b1fbda5bcfee2c21b70f8ec8313d3acd8507c6
Aaron Ingalls <aaroningalls@hotmail.com>
2022-04-27 19:31:35 -0500

    Changes to stuff

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,14 @@
-pub fn test() {
+use crate::rsa;
+use rug::Integer;
 
+pub fn test() {
+    let (public, private) = rsa::rsa_key_gen();
+    println!("public={:?} private={:?}", public, private);
+
+    let c = rsa::rsa_encrypt(&public, Integer::from(4u32));
+    println!("{}", c);
+
+    let m = rsa::rsa_decrypt(&private, c);
+
+    println!("{}", m);
 }
